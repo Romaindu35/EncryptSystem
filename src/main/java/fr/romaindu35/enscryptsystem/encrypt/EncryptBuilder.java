79754ec4554e5a2fbb5738b0d165c4289d0dfec5
Romaindu35 <romain.sai-tanguy@sfr.fr>
2020-08-29 13:24:09 +0200

    Added JavaDoc for the EnscryptBuilder class

--- src/main/java/fr/romaindu35/enscryptsystem/encrypt/EncryptBuilder.java
+++ src/main/java/fr/romaindu35/enscryptsystem/encrypt/EncryptBuilder.java
@@ -12,21 +12,40 @@ public class EncryptBuilder {
     private Keys keys = new Keys();
     private CipherMod cipherMod = CipherMod.ENCRYPT_MODE;
 
+    /**
+     *
+     * @param inputFile : file that will be encrypted
+     * @param outputFile : file destination once encrypted
+     */
     public EncryptBuilder(File inputFile, File outputFile) {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
     }
 
+    /**
+     *
+     * @param keys : take a new Keys : Allows you to define an encryption key
+     * @return : the instance of the class
+     */
     public EncryptBuilder setKeys(Keys keys) {
         this.keys = keys;
         return this;
     }
 
+    /**
+     *
+     * @param cipherMod : Takes a Cipher and allows to define the encryption mode: encrypt or decrypter
+     * @return : the instance of the class
+     */
     public EncryptBuilder setCipherMod(CipherMod cipherMod) {
         this.cipherMod = cipherMod;
         return this;
     }
 
+    /**
+     *
+     * @return : Return an Encrypt object from the EncryptBuilder parameters
+     */
     public Encrypt build() {
         return new Encrypt(inputFile, outputFile, keys, cipherMod);
     }
